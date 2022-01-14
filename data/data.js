0806863b6be3ed5a555f9ceb92c1fb4f8ec914a6
ricardo-ventzke <fkondem@gmail.com>
2022-01-14 20:40:17 +0100

    update files formats

--- data/data.js
+++ data/data.js
@@ -1,53 +1,49 @@
+module.exports = {
+  questions,
+  rightAns,
+};
 
-module.exports={
-    questions,
-    rightAns
-}
+export const questions = [
+  {
+    question: 'what is and african meal ',
+    answers: ['Donner', 'Achu', 'kayam', 'spines'],
+  },
+  {
+    question: ' Most sexy woman ',
+    answers: ['Beyounce', 'Ben ', 'Lucy', 'Joy'],
+  },
+  {
+    question: ' How much is my tv',
+    answers: ['100 euros ', '200 euros', '20 euros ', '10 euros '],
+  },
+  {
+    question: ' whats the name of my first girl friend ',
+    answers: [' Ann', ' Eqios', 'Beryl', ' Ogo'],
+  },
+  {
+    question: 'who develop facebook',
+    answers: ['Mark zukenberg', 'etoo', 'messi', 'ricky'],
+  },
+  {
+    question: ' whats the name of my mama ',
+    answers: ['justine ', 'kate ', 'mama', 'joy'],
+  },
+  {
+    question: 'How old is Neymar',
+    answers: ['1', '30', '29', '9'],
+  },
+  {
+    question: 'Is  and African country',
+    answers: ['Cameroon', 'Germany', 'Spain', 'France '],
+  },
+  {
+    question: 'who develop Instagram',
+    answers: ['Mark zukenberg', 'etoo', 'messi', 'ricky'],
+  },
+  {
+    question: 'Best fotballer in ther world ? ',
+    answers: ['ronaldo', 'etoo', 'messi', 'ricky'],
+  },
+];
 
-export  const questions=[
-    {
-        question:"what is and african meal ",
-        answers:["Donner", "Achu","kayam","spines"]
-    },
-    {
-        question: " Most sexy woman ",
-        answers: ["Beyounce", "Ben ", "Lucy", "Joy"]
-    },
-    {
-        question: " How much is my tv",
-        answers: ["100 euros ", "200 euros", "20 euros ", "10 euros "]
-    },
-    {
-        question: " whats the name of my first girl friend ",
-        answers: [" Ann", " Eqios", "Beryl", " Ogo"]
-    },
-    {
-        question: "who develop facebook",
-        answers: ["Mark zukenberg", "etoo", "messi", "ricky"]
-    },
-    {
-        question: " whats the name of my mama ",
-        answers: ["justine ", "kate ", "mama", "joy"]
-    },
-    {
-        question: "How old is Neymar",
-        answers: ["1", "30", "29", "9"]
-    },
-    {
-        question: "Is  and African country",
-        answers: ["Cameroon", "Germany", "Spain", "France "]
-    },
-    {
-        question: "who develop Instagram",
-        answers: ["Mark zukenberg", "etoo", "messi", "ricky"]
-    },
-    {
-        question: "Best fotballer in ther world ? ",
-        answers: ["ronaldo", "etoo", "messi", "ricky"]
-    }
-]
-
-
-export const rightAns=[
-  1,2,4,2,4,2,1,2,3,2  
-]
+export const rightAns = [1, 2, 4, 2, 4, 2, 1, 2, 3, 2];
--- script.js
+++ script.js
@@ -1,163 +1,143 @@
 const questions = [
-    {
-        question: "what is and african meal ?",
-        answers: ["Donner", "Achu", "kayam", "spines"]
-    },
-    {
-        question: " Most sexy woman ? ",
-        answers: ["Beyounce", "Ben ", "Lucy", "Joy"]
-    },
-    {
-        question: " How much is my tv ?",
-        answers: ["100 euros ", "200 euros", "20 euros ", "10 euros "]
-    },
-    {
-        question: " whats the name of my first girl friend ? ",
-        answers: [" Ann", " Eqios", "Beryl", " Ogo"]
-    },
-    {
-        question: "who develop facebook ?",
-        answers: ["Mark zukenberg", "etoo", "messi", "ricky"]
-    },
-    {
-        question: " whats the name of my mama ? ",
-        answers: ["justine ", "kate ", "mama", "joy"]
-    },
-    {
-        question: "How old is Neymar ?",
-        answers: ["1", "30", "29", "9"]
-    },
-    {
-        question: "Is  and African country ?",
-        answers: ["Cameroon", "Germany", "Spain", "France "]
-    },
-    {
-        question: "who develop Instagram ?",
-        answers: ["Mark zukenberg", "etoo", "messi", "ricky"]
-    },
-    {
-        question: "Best fotballer in ther world ? ",
-        answers: ["ronaldo", "etoo", "messi", "ricky"]
-    }
-]
+  {
+    question: 'what is and african meal ?',
+    answers: ['Donner', 'Achu', 'kayam', 'spines'],
+  },
+  {
+    question: ' Most sexy woman ? ',
+    answers: ['Beyounce', 'Ben ', 'Lucy', 'Joy'],
+  },
+  {
+    question: ' How much is my tv ?',
+    answers: ['100 euros ', '200 euros', '20 euros ', '10 euros '],
+  },
+  {
+    question: ' whats the name of my first girl friend ? ',
+    answers: [' Ann', ' Eqios', 'Beryl', ' Ogo'],
+  },
+  {
+    question: 'who develop facebook ?',
+    answers: ['Mark zukenberg', 'etoo', 'messi', 'ricky'],
+  },
+  {
+    question: ' whats the name of my mama ? ',
+    answers: ['justine ', 'kate ', 'mama', 'joy'],
+  },
+  {
+    question: 'How old is Neymar ?',
+    answers: ['1', '30', '29', '9'],
+  },
+  {
+    question: 'Is  and African country ?',
+    answers: ['Cameroon', 'Germany', 'Spain', 'France '],
+  },
+  {
+    question: 'who develop Instagram ?',
+    answers: ['Mark zukenberg', 'etoo', 'messi', 'ricky'],
+  },
+  {
+    question: 'Best fotballer in ther world ? ',
+    answers: ['ronaldo', 'etoo', 'messi', 'ricky'],
+  },
+];
 
+const rightAns = [1, 0, 1, 3, 0, 0, 2, 0, 0, 2];
 
-const rightAns = [
-    1, 0, 1, 3, 0, 0, 2, 0, 0, 2
-]
+let userAns = [];
 
-let userAns=[]
-
-
-let sum = 0
+let sum = 0;
 let questionLength = questions.length;
 let questionNumber = 0;
-let totalScore= 0;
-let answersLength= 3;
+let totalScore = 0;
+let answersLength = 3;
 let initialQuestionText = questions[questionNumber].question;
-let html = "";
-let questionAnswers = document.querySelector(".answers");
-let questionText = document.getElementById("question__text");
-let nextButton = document.querySelector(".nextButton");
-let resetBtn = document.querySelector(".reset-btn");
-let selectedAnwser = document.querySelector(".answers");
-let score = document.querySelector(".score");
-let btn = document.getElementsByClassName("btn");
+let html = '';
+let questionAnswers = document.querySelector('.answers');
+let questionText = document.getElementById('question__text');
+let nextButton = document.querySelector('.nextButton');
+let resetBtn = document.querySelector('.reset-btn');
+let selectedAnwser = document.querySelector('.answers');
+let score = document.querySelector('.score');
+let btn = document.getElementsByClassName('btn');
 let seleted = true;
 
 function displayAnswers() {
-    for (i = 0; i <= answersLength ; i++) {
-        questionAnswers.innerHTML = html += `
+  for (i = 0; i <= answersLength; i++) {
+    questionAnswers.innerHTML = html += `
                  <button class="btn">${questions[questionNumber].answers[i]}</button>
-               `
-    }
+               `;
+  }
 }
 
+const InitializeApp = () => {
+  questionAnswers.innerHTML = '';
+  html = '';
+  questionNumber = 0;
+  questionText.innerHTML = initialQuestionText;
+  score.innerHTML = 0;
+  displayAnswers();
+  seleted = false;
+};
 
-const InitializeApp=()=>{
-
-    questionAnswers.innerHTML = "";
-    html = "";
-    questionNumber = 0
-    questionText.innerHTML = initialQuestionText;
-    score.innerHTML = 0;
-    displayAnswers()
-    seleted = false;
-}
-
-
-document.addEventListener("DOMContentLoaded",()=>{
-    InitializeApp()
- })
-
-
-
-
-nextButton.addEventListener("click", function () {
- 
-    Array.from(btn, (button,index) => {
-        if (button.classList.contains("selected-answer")){
-            // console.log(index, button)
-            userAns.push(index);
-   
-        }
-    });
-
-try {
-    if (questionNumber == questionLength-1){
-        questionNumber=-1 ;
-        for (let i = 0; i <=rightAns.length-1;i++){
-            if (rightAns[i] == userAns[i]){
-                  totalScore+=1 ;               
-            }else{
-                totalScore = totalScore;
-            }
-        }
-      
-      
-        score.innerHTML = totalScore;
-        alert(`you have ${totalScore} correct of ${rightAns.length || userAns.length}`);
-    
-        // totalScore=0;
-        userAns.length=0
-    }
-    if (seleted){
-        questionNumber++;
-        questionText.innerHTML = questions[questionNumber].question;
-        questionAnswers.innerHTML = "";
-        html = ""
-        displayAnswers();
-        seleted = false;
-    }else{
-        alert("Please select Answer")
-    }
-
-
-    // userAns.push(e.target.innerHTML)
-
-} catch (error) {
- console.log(error)
-
-}
-
+document.addEventListener('DOMContentLoaded', () => {
+  InitializeApp();
 });
 
-const restApp=()=>{
-    InitializeApp();
-    
-}
+nextButton.addEventListener('click', function () {
+  Array.from(btn, (button, index) => {
+    if (button.classList.contains('selected-answer')) {
+      // console.log(index, button)
+      userAns.push(index);
+    }
+  });
 
-resetBtn.addEventListener("click", restApp);
+  try {
+    if (questionNumber == questionLength - 1) {
+      questionNumber = -1;
+      for (let i = 0; i <= rightAns.length - 1; i++) {
+        if (rightAns[i] == userAns[i]) {
+          totalScore += 1;
+        } else {
+          totalScore = totalScore;
+        }
+      }
 
-selectedAnwser.addEventListener("click",function(e){
-    
-    if (e.target.className =="answers") return ;
-    Array.from(btn, button => {
-        button.classList.remove("selected-answer");
-    });
-    e.target.classList.toggle("selected-answer");
-    seleted = true;
-})
+      score.innerHTML = totalScore;
+      alert(
+        `you have ${totalScore} correct of ${rightAns.length || userAns.length}`
+      );
 
+      // totalScore=0;
+      userAns.length = 0;
+    }
+    if (seleted) {
+      questionNumber++;
+      questionText.innerHTML = questions[questionNumber].question;
+      questionAnswers.innerHTML = '';
+      html = '';
+      displayAnswers();
+      seleted = false;
+    } else {
+      alert('Please select Answer');
+    }
 
+    // userAns.push(e.target.innerHTML)
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+const restApp = () => {
+  InitializeApp();
+};
+
+resetBtn.addEventListener('click', restApp);
+
+selectedAnwser.addEventListener('click', function (e) {
+  if (e.target.className == 'answers') return;
+  Array.from(btn, (button) => {
+    button.classList.remove('selected-answer');
+  });
+  e.target.classList.toggle('selected-answer');
+  seleted = true;
+});
